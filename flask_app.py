02f87cfbf3d067da10a686e79674bfd2003260b3
klayton-leandro <klayton_leandro54@hotmail.com>
2020-03-12 00:57:27 +0000

    inser folder templates config env flask render

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def render_static(page_name):
-    return render_template('%.html' % page_name)
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/home")
+def index():
+  return render_template('index.html')
